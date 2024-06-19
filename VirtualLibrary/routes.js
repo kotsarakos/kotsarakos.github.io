04ec3074d1b1a62876201642863f5697ae937c18
Konstantinos Kotsaras <133283667+kotsarakos@users.noreply.github.com>
2024-06-20 01:15:06 +0300

    Delete VirtualLibrary directory

--- VirtualLibrary/routes.js
+++
@@ -1,7 +0,0 @@
-module.exports.configure = (app) => {
-    // Route for the home page
-    app.get('/', (req, res) => {
-        // Send the index.html file located in the public directory as the response
-        res.sendFile(path.join(__dirname, 'public', 'index.html'));
-    });
-};
